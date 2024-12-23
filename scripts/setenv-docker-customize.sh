fb06d9de3cc2e54516185a1d33d2c79a9fb17cfd
Houssem Ben Ali <hbenali.tn@gmail.com>
2024-12-23 10:56:59 +0100

    Fix hsqldb persistence with docker volumes
    (cherry picked from commit 229cea4971bf954b7bb3225dc55f2315d54085ec)

--- scripts/setenv-docker-customize.sh
+++ scripts/setenv-docker-customize.sh
@@ -168,7 +168,8 @@ MEEDS_ES_URL="${MEEDS_ES_SCHEME}://${MEEDS_ES_HOST}:${MEEDS_ES_PORT}"
 
 # Mapping with sentenv.sh 
 [ ! -z "${MEEDS_JVM_SIZE_MAX}" ] && EXO_JVM_SIZE_MAX="${MEEDS_JVM_SIZE_MAX}"
-[ ! -z "${MEEDS_JVM_SIZE_MIN}" ] && EXO_JVM_SIZE_MIN="${MEEDS_JVM_SIZE_MIN}"
+[ ! -z "${MEEDS_JVM_SIZE_MAX}" ] && EXO_JVM_SIZE_MAX="${MEEDS_JVM_SIZE_MAX}"
+[ ! -z "${MEEDS_DATA_DIR}" ] && EXO_DATA_DIR="${MEEDS_JVM_SIZE_MIN}"
 
 
 set -u		# REACTIVATE unbound variable check
